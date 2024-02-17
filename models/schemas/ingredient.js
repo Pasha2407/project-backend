8f039cf27277a4fe2878b471484a71d57023e748
Pasha2407 <70815888+Pasha2407@users.noreply.github.com>
2024-02-17 03:29:01 +0200

    resolve conflict ingredient.js

--- models/schemas/ingredient.js
+++ models/schemas/ingredient.js
@@ -5,6 +5,33 @@ const ingredientSchema = new mongoose.Schema(
         title: {
             type: String,
         },
+        ingredientThumb: {
+            type: String,
+        },
+        "thumb-medium": {
+            type: String,
+        },
+        "thumb-small": {
+            type: String,
+        },
+        abv: {
+            type: String,
+        },
+        alcohol: {
+            type: String,
+        },
+        description: {
+            type: String,
+        },
+        type: {
+            type: String,
+        },
+        flavour: {
+            type: String,
+        },
+        country: {
+            type: String,
+        },
     },
     {
         versionKey: false,
