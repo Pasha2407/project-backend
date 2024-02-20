ec29dfdca3733210662d707d0d3f66688edb6b4e
dozeran <dozeran@proton.me>
2024-02-20 21:47:00 +0200

    popular limit

--- controllers/drinks/getPopular.js
+++ controllers/drinks/getPopular.js
@@ -2,14 +2,20 @@ const recipeModel = require("../../models/schemas/recipe");
 
 async function getPopular(req, res) {
   const adult = req.user.adult;
+
+  const limit = 4;
+
   const filter = { owner: null };
   if (!adult) {
     filter.alcoholic = "Non alcoholic";
   }
 
-  const result = await recipeModel.find({ ...filter }).sort({
-    popularity: -1,
-  });
+  const result = await recipeModel
+    .find({ ...filter })
+    .sort({
+      popularity: -1,
+    })
+    .limit(limit);
 
   res.json(result);
 }
