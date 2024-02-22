d3862d00ac1cfbdd3f7b7018c1be1ef5e54adcc6
dozeran <dozeran@proton.me>
2024-02-22 19:43:52 +0200

    favorite pagination

--- controllers/drinks/getFavorite.js
+++ controllers/drinks/getFavorite.js
@@ -1,11 +1,14 @@
 const recipeModel = require("../../models/schemas/recipe");
 
 async function getFavorite(req, res) {
+  const { page = 1, limit = 9 } = req.query;
+  const skip = (page - 1) * limit;
+
   const id = req.user.id;
   const result = await recipeModel.find({ favorite: id });
   res.json({
     userId: id,
-    data: result,
+    data: result.splice(skip, limit),
   });
 }
 
