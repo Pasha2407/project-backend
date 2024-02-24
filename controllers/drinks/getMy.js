3227d27b5eb565e741e4e69a9e9124b2926fa63d
dozeran <dozeran@proton.me>
2024-02-24 20:26:03 +0200

    pagination totalHits

--- controllers/drinks/getMy.js
+++ controllers/drinks/getMy.js
@@ -2,9 +2,15 @@ const recipeModel = require("../../models/schemas/recipe");
 
 async function getMy(req, res) {
   const id = req.user.id;
+  const { page = 1, limit = 9 } = req.query;
+  const skip = (page - 1) * limit;
+
   const result = await recipeModel.find({ owner: id });
 
-  res.status(200).send({ result });
+  res.status(200).send({
+    totalHits: result.length,
+    data: result.splice(skip, limit),
+  });
 }
 
 module.exports = getMy;
